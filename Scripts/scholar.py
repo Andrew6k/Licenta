29c2e9687db848dc8cc475999e89527d2830f4b2
Andrew6k <75756033+Andrew6k@users.noreply.github.com>
2023-01-31 03:38:25 +0200

    Update scholar.py

--- Scripts/scholar.py
+++ Scripts/scholar.py
@@ -24,30 +24,64 @@ pg = ProxyGenerator()
 pg.FreeProxies()
 scholarly.use_proxy(pg)
 
-# Retrieve the author's data, fill-in, and print
-# Get an iterator for the author results
+id1 = 0
+id2 = 0
 
 data = {}
-data["authors"] = ["Mihaela Breaban"]
+data["authors"] = []
 data["pub"] = []
+data["domains"] = []
+
+domID = {}
 
 search_query = scholarly.search_author('Mihaela Breaban')
 first_author_result = next(search_query)
 author = scholarly.fill(first_author_result )
 print(author)
 
+
 mail = author["name"].lower().replace(" ",".")
 password = get_pass()
 
-values = (author["name"], author["affiliation"], author["email_domain"], int(author["citedby"]), mail, password)
+if author["name"] not in data["authors"]: #se reseteaza la run
+    values = (author["name"], author["affiliation"], author["email_domain"], int(author["citedby"]), mail, password)
 
-insert_query = f"""
-INSERT INTO authors (name, affiliation, email_domain, citations, mail, password)
-VALUES {values}
-"""
+    insert_query = f"""
+    INSERT INTO authors (name, affiliation, email_domain, citations, mail, password)
+    VALUES {values}
+    """
 
-cursor.execute(insert_query)
-conn.commit()
+    cursor.execute(insert_query)
+    conn.commit()
+    data["authors"].append(author["name"]) 
+    id1 = cursor.lastrowid
+
+
+for domain in author["interests"]:
+    # domain = str(interest)
+    # print(domain)
+    if domain not in data["domains"]:
+        #print(domain)
+        values = [domain]
+
+        insert_query = """INSERT INTO domains (domain) VALUES (%s)"""
+        cursor.execute(insert_query, values)
+        conn.commit()
+        data["domains"].append(domain) 
+        # domID[domain] = cursor.lastrowid
+
+    id2 = cursor.lastrowid
+    values = (id1, id2)
+
+    insert_query = f"""
+    INSERT INTO author_domains (author_id, domain_id)
+    VALUES {values}
+    """
+
+    cursor.execute(insert_query)
+    conn.commit()
+    # id2 = cursor.lastrowid
+
 
 cursor.close()
 conn.close()
