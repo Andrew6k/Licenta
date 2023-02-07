688fe6eac593e96c6082e234a54d7d314b3f8b18
Andrew6k <75756033+Andrew6k@users.noreply.github.com>
2023-02-08 00:06:16 +0200

    Update details.php

--- web/details.php
+++ web/details.php
@@ -45,5 +45,39 @@
     </div>
 </div>
 
+<div class="articles">
+    <?php 
+      $nr = intval($id);
+      $sql = "SELECT title, conference, year, citations, link FROM publications JOIN author_publications ON publications.id = author_publications.publication_id  WHERE author_id = '$nr'";
+      $result=mysqli_query($mysqli,$sql);
+        ?>
+        <table align="center" border="1px"">
+        <tr>
+            <th colspan="5"><h2 style="text-align: center;">Publications</h2></th>
+        </tr>
+
+        <t>
+            <th>Title</th>
+            <th>Conference</th>
+            <th>Year</th>
+            <th>Citations</th>
+            <th>Link</th>
+        </t>
+        <?php
+        while($rows=mysqli_fetch_assoc($result))
+        {
+        ?>
+            <tr>
+                <td><?php echo $rows['title']; ?></td>
+                <td><?php echo $rows['conference']; ?></td>
+                <td><?php echo $rows['year']; ?></td>
+                <td><?php echo $rows['citations']; ?></td>
+                <td><?php echo $rows['link']; ?></td>
+            </tr>
+            <?php
+        }
+        ?>
+    </table>
+</div>
 </body>
 </html>
