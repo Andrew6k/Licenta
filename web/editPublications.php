5b3610fb9527602a4d2bb24bfc7005092457ccd1
Andrew6k <75756033+Andrew6k@users.noreply.github.com>
2023-04-10 18:08:03 +0300

    populate form

--- web/editPublications.php
+++ web/editPublications.php
@@ -1,10 +1,10 @@
 <?php
     include('server.php');
 
-    $author_id = $_SESSION['auth_id'];
-    $sql = "SELECT * FROM authors WHERE id = $author_id";
-    $result = mysqli_query($mysqli, $sql);
-    $author = mysqli_fetch_assoc($result);
+    // $author_id = $_SESSION['auth_id'];
+    // $sql = "SELECT * FROM authors WHERE id = $author_id";
+    // $result = mysqli_query($mysqli, $sql);
+    // $author = mysqli_fetch_assoc($result);
 
     $id = $_GET["id"];
     $sql = "SELECT * FROM publications WHERE id=$id";
@@ -32,13 +32,13 @@
   <form action="update_publications.php" method="post">
   <input type="hidden" name="author_id" value="<?php echo $author_id; ?>">
   <label for="publication_title">Publication Title:</label>
-  <input type="text" id="publication_title" name="publication_title" value="<?php echo $publication_title; ?>">
-  <label for="publication_date">Publication Date:</label>
-  <input type="date" id="publication_date" name="publication_date" value="<?php echo $publication_date; ?>">
+  <input type="text" id="publication_title" name="publication_title" value="<?php echo $title; ?>">
+  <label for="publication_year">Publication Year:</label>
+  <input type="number" id="year" name="year" value="<?php echo htmlspecialchars($year); ?>">
   <label for="publication_journal">Publication Journal:</label>
-  <input type="text" id="publication_journal" name="publication_journal" value="<?php echo $publication_journal; ?>">
+  <input type="text" id="publication_journal" name="publication_journal" value="<?php echo $conference; ?>">
   <label for="citation_count">Citation Count:</label>
-  <input type="number" id="citation_count" name="citation_count" value="<?php echo $citation_count; ?>">
+  <input type="number" id="citation_count" name="citation_count" value="<?php echo htmlspecialchars($citations); ?>">
   <input type="submit" value="Save">
 </form>
   </body>
