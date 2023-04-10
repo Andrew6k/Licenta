1cc78f5bee63b76ee739b560c97ee6bc9aa68f88
Andrew6k <75756033+Andrew6k@users.noreply.github.com>
2023-04-10 19:21:57 +0300

    updateFormdb

--- web/editPublications.php
+++ web/editPublications.php
@@ -26,20 +26,47 @@
 <html>
   <head>
     <title>Edit Form</title>
-    <link rel="stylesheet" type="text/css" href="form.css">
+    <!-- <link rel="stylesheet" href="style.css"> -->
+    <link rel="stylesheet" href="form.css">
   </head>
+  <div class="nav">
+<?php if(isset($_SESSION['username']) and isset($_SESSION['isadmin']) and ($_SESSION['isadmin'] == 0)) {
+      $name = $_SESSION['username'];
+      echo "<a href='logout.php'>Logout</a>";
+      
+      echo "<a href='mypage.php'>$name</a>";
+      
+    }elseif (isset($_SESSION['username']) and isset($_SESSION['isadmin']) and ($_SESSION['isadmin'] == 1)) {
+      $name = $_SESSION['username'];
+      echo "<a href='logout.php'>Logout</a>";
+      
+      echo "<a href='adminPage.php'>$name</a>";
+    }else {
+      echo "<a href='login.php'>Log in</a>";
+    }
+    ?>
+    <a href="homepage.php">Home</a>
+</div>
   <body>
+    <div class="background">
   <form action="update_publications.php" method="post">
-  <input type="hidden" name="author_id" value="<?php echo $author_id; ?>">
+  <input type="hidden" name="publication_id" value="<?php echo $id; ?>">
   <label for="publication_title">Publication Title:</label>
   <input type="text" id="publication_title" name="publication_title" value="<?php echo $title; ?>">
   <label for="publication_year">Publication Year:</label>
   <input type="number" id="year" name="year" value="<?php echo htmlspecialchars($year); ?>">
-  <label for="publication_journal">Publication Journal:</label>
-  <input type="text" id="publication_journal" name="publication_journal" value="<?php echo $conference; ?>">
+  <label for="publication_conference">Publication Conference:</label>
+  <input type="text" id="publication_conference" name="publication_conference" value="<?php echo $conference; ?>">
   <label for="citation_count">Citation Count:</label>
   <input type="number" id="citation_count" name="citation_count" value="<?php echo htmlspecialchars($citations); ?>">
   <input type="submit" value="Save">
 </form>
+<div class="msj">
+  <?php if (isset($_SESSION['messageUp'])) {
+    echo $_SESSION['messageUp'];
+    unset($_SESSION['messageUp']);
+          }?>
+ </div>
+</div>
   </body>
 </html>
---
+++ web/update_publications.php
@@ -0,0 +1,29 @@
+<?php
+    include('server.php');
+
+    $publication_id = $_POST["publication_id"];
+    $publication_title = $_POST["publication_title"];
+    $publication_year = $_POST['year'];
+    $publication_conference = $_POST['publication_conference'];
+    $citation_count = $_POST['citation_count'];
+
+    $publication_id = $mysqli->real_escape_string($publication_id);
+    $publication_title = $mysqli->real_escape_string($publication_title);
+    $publication_year = $mysqli->real_escape_string($publication_year);
+    $publication_conference = $mysqli->real_escape_string($publication_conference);
+    $citation_count = $mysqli->real_escape_string($citation_count);
+
+    // echo $publication_conference, $publication_title, $publication_year, $citation_count;
+
+    $query = "UPDATE publications SET title='$publication_title', year='$publication_year', conference='$publication_conference', citations='$citation_count' WHERE id='$publication_id'";
+    $result = $mysqli->query($query);   
+
+    if ($result) {
+         $_SESSION['messageUp'] = "<p>Publication information updated successfully!</p>";
+      } else {
+         $_SESSION['messageUp'] = "<p>Error updating publication information: " . $mysqli>error . "</p>";
+      }
+
+    header("Location: editPublications.php"."?id=".$publication_id);
+    exit();
+    ?>
